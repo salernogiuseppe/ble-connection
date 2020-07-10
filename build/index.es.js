12210dca577ec23d86f77dbb2c7cbb83a7a5d07b
Giuseppe Salerno <gi.salerno@reply.it>
2020-07-10 17:50:52 +0200

    add module ble-connection

---
+++ build/index.es.js
@@ -0,0 +1,5 @@
+var sum = function (a, b) { return a + b; };
+console.log('test somma:', sum(10, 5));
+
+export { sum };
+//# sourceMappingURL=index.es.js.map
---
+++ build/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var sum = function (a, b) { return a + b; };
+console.log('test somma:', sum(10, 5));
+
+exports.sum = sum;
+//# sourceMappingURL=index.js.map
